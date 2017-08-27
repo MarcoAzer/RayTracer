f93a2200af093d0f8dce114b2888809b64d54866
Marco Azer <marco.azer@uwaterloo.ca>
2017-08-27 17:21:19 -0400

    Ignored images

--- src/A4.cpp
+++ src/A4.cpp
@@ -16,6 +16,100 @@ enum State
     nostate, background, shadow, color
 };
 
+glm::vec3 Get_Pixel_Color( SceneNode* root, 
+                           const std::list<Light *> & lights, 
+                           const glm::vec3 & ambient,
+                           Ray& ray ){
+
+    State pixel_state = background;
+    glm::vec3 col;
+    float t = INT_MAX;
+    SceneNode* obj_intersect = NULL;
+
+    // Loop through all objects to find the one that intersects
+    for( SceneNode* child : root->children )
+    {
+        float new_t = child->get_t( ray );
+
+        if( new_t > 0 && new_t < t )
+        {
+            t = new_t;
+            obj_intersect = child;
+        }
+    }
+
+    if( t == INT_MAX )
+    {
+        pixel_state = background;
+    }
+    else
+    {
+        GeometryNode* g_node = (GeometryNode*)obj_intersect;
+        PhongMaterial* phong = (PhongMaterial*)g_node->m_material;
+        glm::vec3 p = ray.origin + t * ray.dir;
+
+        for( list<Light*>::const_iterator it = lights.begin(); it != lights.end(); it++ )
+        {
+            glm::vec3 light_dir = p - (*it)->position;
+            float light_dist = sqrt( light_dir.x * light_dir.x + light_dir.y * light_dir.y + light_dir.z * light_dir.z );
+            light_dir = glm::normalize( light_dir );
+            Ray light_ray( (*it)->position, light_dir );
+            float light_t = root->nh_intersect( light_ray );
+
+            const float epsilon = 0.01;
+
+            glm::vec3 N = glm::normalize( g_node->m_primitive->normal( p ) );
+            p = p + epsilon * N;
+                 
+            // Test light intersection
+            // Object between object and light
+            if( light_t == 0 )
+            {
+            }
+            else if( abs( light_t - light_dist ) < epsilon /*|| g_node->m_primitive->epsilon_check( p )*/ ) 
+            {
+                pixel_state = color;
+
+                // Lambertian
+                float l_dot_n = glm::dot( -1 * light_dir, N ) / ( (*it)->falloff[0] + light_dist * (*it)->falloff[1] + light_dist * (*it)->falloff[2] * (*it)->falloff[2] );
+                col.r = phong->m_kd.r * (*it)->colour.r * l_dot_n;
+                col.g = phong->m_kd.g * (*it)->colour.g * l_dot_n;
+                col.b = phong->m_kd.b * (*it)->colour.b * l_dot_n;
+
+                // Spectral 
+                float reflect = 2.0 * glm::dot( light_dir, N );
+                glm::vec3 R = light_dir - reflect * N;
+                float coeff = glm::dot( R, ray.dir );
+                if( coeff < 0.0 )
+                    coeff = 0.0;
+
+                coeff = pow( coeff, phong->m_shininess );
+
+                col.r += coeff * phong->m_ks.r * (*it)->colour.r;
+                col.g += coeff * phong->m_ks.g * (*it)->colour.g;
+                col.b += coeff * phong->m_ks.b * (*it)->colour.b;
+
+                // Ambient
+                col.r += phong->m_kd.r * ambient.r;
+                col.g += phong->m_kd.g * ambient.g;
+                col.b += phong->m_kd.b * ambient.b;
+            }
+            else
+            {
+                if( pixel_state != color )
+                    pixel_state = shadow;
+            }
+        }
+    }
+
+    if( pixel_state == background )
+        return glm::vec3( 0.9, 0.9, 0.9 );
+    else if( pixel_state == shadow )
+        return glm::vec3( 0.0, 0.0, 0.0 );
+    else
+        return col;
+}
+
 void A4_Render(
 		// What to render
 		SceneNode * root,
@@ -57,123 +151,16 @@ void A4_Render(
             // Get direction vector
             glm::vec3 dir = v_for + h * ( ( ( 2 * i) / image.height() ) - 1 ) * v_up + w * ( ( ( 2 * j ) / image.width() ) - 1 ) * v_right;
             dir = glm::normalize( dir );
-            State pixel_state = background;
 
             // Intersection with each object
-            glm::vec3 col;
             Ray ray( l_eye, dir );
-            float t = INT_MAX;
-            SceneNode* obj_intersect = NULL;
 
-            // Loop through all objects to find the one that intersects
-            for( SceneNode* child : root->children )
-            {
-                float new_t = child->get_t( ray );
+            glm::vec3 color = Get_Pixel_Color( root, lights, ambient, ray );
 
-                if( new_t > 0 && new_t < t )
-                {
-                    t = new_t;
-                    obj_intersect = child;
-                }
-            }
+            image( j, i, 0 ) = color.r;
+            image( j, i, 1 ) = color.g;
+            image( j, i, 2 ) = color.b;
 
-            if( t == INT_MAX )
-            {
-                pixel_state = background;
-            }
-            else
-            {
-                GeometryNode* g_node = (GeometryNode*)obj_intersect;
-                PhongMaterial* phong = (PhongMaterial*)g_node->m_material;
-                glm::vec3 p = ray.origin + t * ray.dir;
-
-                for( list<Light*>::const_iterator it = lights.begin(); it != lights.end(); it++ )
-                {
-                    glm::vec3 light_dir = p - (*it)->position;
-                    float light_dist = sqrt( light_dir.x * light_dir.x + light_dir.y * light_dir.y + light_dir.z * light_dir.z );
-                    light_dir = glm::normalize( light_dir );
-                    Ray light_ray( (*it)->position, light_dir );
-                    float light_t = root->nh_intersect( light_ray );
-
-                    const float epsilon = 0.01;
-
-                    glm::vec3 N = glm::normalize( g_node->m_primitive->normal( p ) );
-                    p = p + epsilon * N;
-                         
-                    // Test light intersection
-                    // Object between object and light
-                    if( light_t == 0 )
-                    {
-                    }
-                    else if( abs( light_t - light_dist ) < epsilon /*|| g_node->m_primitive->epsilon_check( p )*/ ) 
-                    {
-                        pixel_state = color;
-
-                        // Lambertian
-                        float l_dot_n = glm::dot( -1 * light_dir, N ) / ( (*it)->falloff[0] + light_dist * (*it)->falloff[1] + light_dist * (*it)->falloff[2] * (*it)->falloff[2] );
-                        col.r = phong->m_kd.r * (*it)->colour.r * l_dot_n;
-                        col.g = phong->m_kd.g * (*it)->colour.g * l_dot_n;
-                        col.b = phong->m_kd.b * (*it)->colour.b * l_dot_n;
-
-                        // Spectral 
-                        float reflect = 2.0 * glm::dot( light_dir, N );
-                        glm::vec3 R = light_dir - reflect * N;
-                        float coeff = glm::dot( R, dir );
-                        if( coeff < 0.0 )
-                            coeff = 0.0;
-
-                        coeff = pow( coeff, phong->m_shininess );
-
-                        col.r += coeff * phong->m_ks.r * (*it)->colour.r;
-                        col.g += coeff * phong->m_ks.g * (*it)->colour.g;
-                        col.b += coeff * phong->m_ks.b * (*it)->colour.b;
-
-                        // Ambient
-                        col.r += phong->m_kd.r * ambient.r;
-                        col.g += phong->m_kd.g * ambient.g;
-                        col.b += phong->m_kd.b * ambient.b;
-                    }
-                    else
-                    {
-                        if( pixel_state != color )
-                            pixel_state = shadow;
-                    }
-                }
-            }
-
-            if( pixel_state == background )
-            {
-                if( i < background_section )
-                {
-                    image( j, i, 0 ) = 0.0 + ( 2 * i / 255 );
-                    image( j, i, 1 ) = 0.0 + ( 2 * i / 255 );
-                    image( j, i, 2 ) = 0.5 + ( 2 * i / 255 );
-                }
-                else if( i < background_section * 2 )
-                {
-                    image( j, i, 0 ) = 0.0 + ( 2 * i / 255 );
-                    image( j, i, 1 ) = 0.0 + ( 2 * i / 255 );
-                    image( j, i, 2 ) = 0.5 + ( 2 * i / 255 );
-                }
-                else
-                {
-                    image( j, i, 0 ) = 139.0/255.0 + ( 255.0 - i )/ 255.0;
-                    image( j, i, 1 ) =  69.0/255.0 + ( 255.0 - i )/ 255.0;
-                    image( j, i, 2 ) =  19.0/255.0 + ( 255.0 - i )/ 255.0;
-                }
-            }
-            else if( pixel_state == shadow )
-            {
-                image( j, i, 0 ) = 0.0;
-                image( j, i, 1 ) = 0.0;
-                image( j, i, 2 ) = 0.0;
-            }
-            else
-            {
-                image( j, i, 0 ) = col.r;
-                image( j, i, 1 ) = col.g;
-                image( j, i, 2 ) = col.b;
-            }
         }
         cout << "\r";
         cout << int(i / image.height() * 100) << "%" << std::flush;
